34bb4b512fcd62e9123a65f14526bf923a1f8d8a
Miah Harper <harpermiah@yahoo.com>
2022-07-05 17:39:52 -0400

    finished kanye west exercise

---
+++ KayneWest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KayneWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new findQuote(client);
+
+            
+
+            Console.WriteLine("Would you like to hear the ramblings of Kanye West and Ron Swanson?");
+
+           
+            var userAnswer = Console.ReadLine().ToLower();
+
+            Console.WriteLine();
+
+            int i = 0;
+
+            if (userAnswer == "yes")
+            {
+
+                
+
+                do
+                {
+                    Console.WriteLine($"Kanye says: {quote.KanyeWest()}");
+                    Console.WriteLine();
+                    Console.WriteLine($"Ron Swanson returns with: {quote.RonSwanson()}");
+                    Console.WriteLine();
+
+
+
+                    i++;
+                }
+
+                while (i < 5);
+                {
+                   
+                }
+                
+            }
+
+            if (userAnswer == "no")
+            {
+
+                Console.WriteLine("I'd wish you best of luck, but I believe luck is a concept created by the weak to explain their failures.");
+
+            }
+
+
+
+        }
+
+        
+
+    }
+}
+
---
+++ KayneWest/findQuote.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace KayneWest
+{
+    public class findQuote
+    {
+        private  HttpClient _client;
+
+        public findQuote(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KanyeWest()
+        {
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+        }
+    }
+}
+
